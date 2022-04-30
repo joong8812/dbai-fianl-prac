531be76e76818343f570b958da91ff9c9e7aefa7
joong8812 <joong8812@google.com>
2022-04-30 10:57:38 +0900

    first commit

---
+++ client/modules/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
+import register, { registerSaga } from './auth/register';
+import login, { loginSaga } from './auth/login';
+
+const rootReducer = combineReducers({
+  register, login
+
+});
+
+export function* rootSaga() {
+  yield all([ registerSaga(), loginSaga()]);
+}
+
+export default rootReducer;
